12965ef67fc6ae07d97430568b552ad5b28aed3c
Noor AlHadidi <noor.ahmed.alhadidi@gmail.com>
2025-07-19 01:00:50 +0300

    generated components and services, implemented article interface

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HotTopicsComponent } from './components/hot-topics/hot-topics.component';
+import { LatestNewsComponent } from './components/latest-news/latest-news.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HotTopicsComponent,
+    LatestNewsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/latest-news/latest-news.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-news',
+  imports: [],
+  templateUrl: './latest-news.component.html',
+  styleUrl: './latest-news.component.css'
+})
+export class LatestNewsComponent {
+
+}
