1f7377d2bafd9c447e2572352887549f24faff9d
Noor AlHadidi <noor.ahmed.alhadidi@gmail.com>
2025-07-19 16:23:23 +0300

    added latest news retrieval function in service

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -8,12 +8,13 @@ import { Article } from '../models/article.model';
 })
 export class NewsService {
   private apiKey = 'db303a2c63824c7ab90c76f1eb36628d';
-  private apiUrl = '/v2';;
+  private apiUrl = '/v2';
 
   constructor(private http: HttpClient) {}
 
   getHotTopic(): Observable<Article | null> {
-    var hotTopicUrl = `${this.apiUrl}/top-headlines`;
+    const hotTopicUrl = `${this.apiUrl}/top-headlines`;
+
     const params = {
       language: 'en',
       pageSize: '1',
@@ -24,9 +25,30 @@ export class NewsService {
       map((response) => response.articles[0]),
       tap((article) => console.log('[NewsService] Hot topic:', article)),
       catchError((error) => {
-        console.error('[NewsService] Error:', error);
+        console.error('[NewsService] Error (hot topic):', error);
         return of(null);
       })
     );
+  }
+
+  getLatestNews(): Observable<Article[] | null> {
+    const latestNewsUrl = `${this.apiUrl}/everything`;
+
+    const params = {
+      language: 'en',
+      pageSize: '8',
+      apiKey: this.apiKey,
+    };
+
+    return this.http
+      .get<{ articles: Article[] }>(latestNewsUrl, { params })
+      .pipe(
+        map((response) => response.articles),
+        tap((articles) => console.log('[NewsService] Latest news:', articles)),
+        catchError((error) => {
+          console.error('[NewsService] Error (latest news):', error);
+          return of(null);
+        })
+      );
   }
 }
